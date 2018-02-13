58e25fd0c2ef261b48cffc5d822f89799c713af4
zplaxx <zplaxx.cmp@gmail.com>
2018-02-12 21:21:56 -0500

    Linearalized the diagonals in the Pieces class.

--- Logic/Pieces/Pieces.py
+++ Logic/Pieces/Pieces.py
@@ -1,4 +1,5 @@
 from Logic.Chessboard import convert_file
+
 """
 Parent Class for all the pieces.
 
@@ -20,6 +21,7 @@ Method:
     move
 
 """
+file_value = dict(a=1, b=2, c=3, d=4, e=5, f=6, g=7, h=8)
 
 
 class Pieces:
@@ -37,3 +39,60 @@ class Pieces:
         self.type = piece_type
         self.value = value
         self.color = color
+
+    '''
+    Remove r and override it in children class instead.
+    '''
+    # Top Right
+    def dig_top_right(self, r):
+        line = []
+        x = convert_file(self.position[0])
+        y = int(self.position[1])
+
+        for i in range(1, r):
+            if x + i <= 8:
+                line.append(convert_file(x + i) + str(y + i))
+            else:
+                break
+        return line
+
+    # Top Left
+    def dig_top_left(self, r):
+        line = []
+        x = convert_file(self.position[0])
+        y = int(self.position[1])
+
+        for i in range(1, r):
+            if y + i <= 8 and x - i > 0:
+                line.append(convert_file(x - i) + str(y + i))
+            else:
+                break
+        return line
+
+    # Bottom Left
+    def dig_bottom_left(self, r):
+        line = []
+        x = convert_file(self.position[0])
+        y = int(self.position[1])
+
+        for i in range(1, r):
+            if x - i > 0:
+                line.append(convert_file(x - i) + str(y - i))
+            else:
+                break
+        return line
+
+    # Bottom Right
+    def dig_bottom_right(self, r):
+        line = []
+        x = convert_file(self.position[0])
+        y = int(self.position[1])
+
+        for i in range(1, r):
+            if x + i <= 8 and y - i > 0:
+                line.append(convert_file(x + i) + str(y - i))
+            else:
+                break
+        return line
+
+
