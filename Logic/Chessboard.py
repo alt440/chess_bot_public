901d8cf767cb62e9e5a84894ec5309daf217549a
Cesare Spinoso <cesare.spinoso@gmail.com>
2018-02-23 01:17:23 -0500

    Possible way of tracking a pieces' position using the chessboard

--- Logic/Chessboard.py
+++ Logic/Chessboard.py
@@ -3,6 +3,21 @@ file = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 file_value = dict(a=1, b=2, c=3, d=4, e=5, f=6, g=7, h=8)
 chess_board = [[str(x) + str(y) for y in rank] for x in file]
 
+# How a return position could work
+def getRank():
+    return rank
+
+def getFile():
+    return file
+
+def getPostion():
+    return [getFile(), getRank()]
+
+# With this position can check the status of every positioj on the board
+def isOccupied(rank, file):
+    return (getRank() == rank and getFile() == file)
+
+
 def printChess():
     print(chess_board)
 
@@ -10,3 +25,4 @@ def convert_file(f):
     if isinstance(f, int):
         return file[f-1]
     return file_value.get(f)
+
