fb775dd9e4310138b07376b7327349dd33280fc3
zplaxx <zplaxx.cmp@gmail.com>
2018-03-02 21:57:16 -0500

    Added the Methods to move in a Straight Line

---
+++ Logic/Pieces/Movement/Line.py
@@ -0,0 +1,88 @@
+"""
+Contains methods that return the possible move for in a line.
+
+There is 4 lines, one for each axis and direction on a cartesian plan
+(right, top, left, bottom).
+"""
+
+from Logic.Chessboard import convert_file
+
+
+def line_right(self):
+    """
+    Returns the possible moves for the Right line.
+
+    :param Piece self: Piece to get move for
+    :return: list line: List of the moves for the Right line
+    :rtype list
+    """
+    line = []
+    x = convert_file(self.position[0])
+    y = int(self.position[1])
+
+    for i in range(1, 8):
+        if x + i <= 8:
+            line.append(convert_file(x + i) + str(y))
+        else:
+            break
+    return line
+
+
+def line_top(self):
+    """
+    Returns the possible moves for the Top line.
+
+    :param Piece self: Piece to get move for
+    :return: list line: List of the moves for the Top line
+    :rtype list
+    """
+    line = []
+    x = convert_file(self.position[0])
+    y = int(self.position[1])
+
+    for i in range(1, 8):
+        if y + i <= 8:
+            line.append(convert_file(x) + str(y + i))
+        else:
+            break
+    return line
+
+
+def line_left(self):
+    """
+    Returns the possible moves for the Left line.
+
+    :param Piece self: Piece to get move for
+    :return: list line: List of the moves for the Left line
+    :rtype list
+    """
+    line = []
+    x = convert_file(self.position[0])
+    y = int(self.position[1])
+
+    for i in range(1, 8):
+        if x - i > 0:
+            line.append(convert_file(x - i) + str(y))
+        else:
+            break
+    return line
+
+
+def line_bottom(self):
+    """
+    Returns the possible moves for the Bottom line.
+
+    :param Piece self: Piece to get move for
+    :return: list line: List of the moves for the Bottom line
+    :rtype list
+    """
+    line = []
+    x = convert_file(self.position[0])
+    y = int(self.position[1])
+
+    for i in range(1, 8):
+        if y - i > 0:
+            line.append(convert_file(x) + str(y - i))
+        else:
+            break
+    return line
