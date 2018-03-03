a75a566a8476a2714bed4c5ded54caccf0873b99
zplaxx <zplaxx.cmp@gmail.com>
2018-03-02 23:16:29 -0500

    Added a method to verify if a position is already occupied

--- Logic/Chessboard.py
+++ Logic/Chessboard.py
@@ -12,6 +12,9 @@ file_value = dict(a=1, b=2, c=3, d=4, e=5, f=6, g=7, h=8)  # Map the file letter
 # Create all the chessboard position in format ("File"+"Rank")
 chess_board = [[str(x) + str(y) for y in rank] for x in file]
 
+# Store the the location for every piece in the format {location : piece}
+piece_location = dict()
+
 
 def convert_file(f):
     """
@@ -23,3 +26,23 @@ def convert_file(f):
     if isinstance(f, int):
         return file[f - 1]
     return file_value.get(f)
+
+
+def is_occupied(self, position):
+    """
+    Returns if their is already a piece on the case.
+
+    :param Piece self: Piece to verify for
+    :param position: Position to be compared to
+    :return: -1 if is occupied by a piece of the same color,
+            0 if their is no piece,
+            1 if their is a piece o the opposite color
+    """
+
+    if position in piece_location.keys():
+        if piece_location[position].COLOR == self.COLOR:
+            return -1
+        else:
+            return 1
+    else:
+        return 0
