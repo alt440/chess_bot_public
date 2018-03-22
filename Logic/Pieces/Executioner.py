4a0f3e29142e583c72ba2af1b76bbf589b825275
alt440 <alexandre.therrien3@gmail.com>
2018-03-22 15:11:38 -0400

    Added necessary code to make a 1-sided game

--- Logic/Pieces/Executioner.py
+++ Logic/Pieces/Executioner.py
@@ -22,27 +22,75 @@ print("Welcome to the simulator")
 White.start_new_game()
 Black.start_new_game()
 
-print("Whites")
-for i in range(1,len(White.white_pieces)):
-    print(White.white_pieces[i])
-    print(White.white_pieces[i].moves())
+"""
+Used to test if the right moves were allowed
+"""
+if user_color == 0:
+    print("Whites")
+    for i in range(1,len(White.white_pieces)):
+        print(White.white_pieces[i].position + ' ' + White.white_pieces[i].name)
+        print(White.white_pieces[i].moves())
 
-print("Blacks")
-for i in range(1,len(Black.black_pieces)):
-    print(Black.black_pieces[i])
-    print(Black.black_pieces[i].moves())
+if user_color == 1:
+    print("Blacks")
+    for i in range(1,len(Black.black_pieces)):
+        print(Black.black_pieces[i].position + ' ' + Black.black_pieces[i].name)
+        print(Black.black_pieces[i].moves())
 
+print("Choose a move to make. First indicate the initial position of the object and mention the new position.")
 """
 Asks the user to enter his moves until checkmate (him or the other player)
 """
 checkmate = False
 
-while checkmate != True:
-    x = input("Enter your next move")
-    print(x)
+while checkmate is not True:
+    x = input("Enter your initial and next move ")
+    x = x.strip()
+
+    concerned_piece = None
+
+    initial_position = x[:2]
+    print(initial_position)
+    position_piece = 'h7'
+    print(position_piece in initial_position)
+    position_piece = "h7"
+    print(position_piece in initial_position)
+
+    if user_color == 0:
+        for i in range(len(White.white_pieces)):
+            #print(White.white_pieces[i].position)
+            if initial_position in White.white_pieces[i].position:
+                print("TRUE")
+                concerned_piece = White.white_pieces[i]
+                break
+
+    elif user_color == 1:
+        for i in range(len(Black.black_pieces)):
+            #print(Black.black_pieces[i].position)
+            if initial_position in Black.black_pieces[i].position:
+                print("TRUE")
+                concerned_piece = Black.black_pieces[i]
+                break
+
+    print(concerned_piece)
+    make_move(concerned_piece, x[2:4])
+
+    if user_color == 0:
+        print("Whites")
+        for i in range(1, len(White.white_pieces)):
+            print(White.white_pieces[i].position + ' ' + White.white_pieces[i].name)
+            print(White.white_pieces[i].moves())
+    elif user_color == 1:
+        print("Blacks")
+        for i in range(1, len(Black.black_pieces)):
+            print(Black.black_pieces[i].position + ' ' + Black.black_pieces[i].name)
+            print(Black.black_pieces[i].moves())
 
     """
     Check in the list of moves which one is most efficient for the AI
     --Return the AI move below
     """
+
+
+
 
