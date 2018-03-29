493558482adb980fd04693ed1fb1576c858b1110
alt440 <alexandre.therrien3@gmail.com>
2018-03-29 11:27:20 -0400

    Error eating pawn in front corrected

--- Logic/Pieces/Pawn.py
+++ Logic/Pieces/Pawn.py
@@ -14,22 +14,26 @@ class Pawn(Pieces):
         return piece_location_chessboard_view[file_pos - 1][rank_pos - 1] is not None \
             and not (piece_location_chessboard_view[file_pos - 1][rank_pos - 1].COLOR == color)
 
+    def is_space_occupied(file_pos, rank_pos):
+        return piece_location_chessboard_view[file_pos - 1][rank_pos - 1] is not None
+
+
     #Firstly, taking in consideration all possible moves.
     def moves(self):
         moves = list()
         file_pos = int(convert_file(self.position[0]))
         rank_pos = int(self.position[1]) #raw
 
-        if self.COLOR == 0 and rank_pos == 2 and is_space_available(file_pos, rank_pos + 2, self.COLOR): #white
+        if self.COLOR == 0 and rank_pos == 2 and not Pawn.is_space_occupied(file_pos, rank_pos + 2): #white
             moves.append(convert_file(file_pos) + str(rank_pos + 2))
 
-        elif rank_pos == 7 and self.COLOR == 1 and is_space_available(file_pos, rank_pos - 2, self.COLOR): #black
+        elif rank_pos == 7 and self.COLOR == 1 and not Pawn.is_space_occupied(file_pos, rank_pos - 2): #black
             moves.append(convert_file(file_pos)+str(rank_pos-2))
 
-        if self.COLOR == 0 and rank_pos+1 <= 8 and is_space_available(file_pos, rank_pos + 1, self.COLOR):
+        if self.COLOR == 0 and rank_pos+1 <= 8 and not Pawn.is_space_occupied(file_pos, rank_pos + 1):
             moves.append(convert_file(file_pos) + str(rank_pos + 1))
 
-        elif self.COLOR == 1 and rank_pos-1 > 0 and is_space_available(file_pos, rank_pos - 1, self.COLOR):
+        elif self.COLOR == 1 and rank_pos-1 > 0 and not Pawn.is_space_occupied(file_pos, rank_pos - 1):
             moves.append(convert_file(file_pos) + str(rank_pos - 1))
 
 
@@ -47,23 +51,6 @@ class Pawn(Pieces):
                 moves.append(convert_file(file_pos - 1) + str(rank_pos - 1))
             if file_pos+1 <= 8 and rank_pos - 1 > 0 and Pawn.possibility_of_capturing(file_pos + 1, rank_pos - 1, self.COLOR):
                 moves.append(convert_file(file_pos + 1) + str(rank_pos - 1))
-
-        """
-                      elif (FirstMove.neg() or self.COLOR == 0):
-                if  (rank_pos +1 <= 8):
-                    if rank_pos +1 > 8:
-                    #Something will happen here
-                    #creat a method
-                    elif rank_pos + 1 <=8:
-                        moves.append(convert_file(file_pos)+str(rank_pos+1))
-           elif (FirstMove.neg() or self.COLOR == 1):
-                if  (rank_pos -1 <= 0):
-                    if rank_pos -1 > 0:
-                    #Something will happen here
-                    #creat a method
-                    elif rank_pos - 1 <=0:
-                        moves.append(convert_file(file_pos)+str(rank_pos-1))
-        """
 
         return moves
 
